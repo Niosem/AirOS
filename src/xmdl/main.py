f3f5cfd8cf8256f7f9d344840eaf3d049c99541a
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2023-05-24 19:36:41 +0200

    added code

--- src/xmdl/main.py
+++ src/xmdl/main.py
@@ -3,12 +3,17 @@ import gethttp
 import os
 import psutil
 
+source = input("What is the package source ? (aur, AirOS, snap, flatpak): ")
 pkg = input("Package: ")
 action = input("Action: ")
 userDo = input("{} {}: ? (y/n)".format(action, pkg))
 
+if (source == "aur") :
+    gethttp.get_package_aur()
+elif (source == "AirOS") :
+    gethttp.get_package()
+
 if (userDo == "y") :
     print("{} undergoing  {}. Hang tight, this may take a while".format(pkg, action))
-    gethttp.get_package_aur()
 else:
     print("Unknown or negative awnser. no changes made to {}, using System Partition".format(pkg))
