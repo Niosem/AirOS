6ba08d3cd658e851a5cc0d5357d262212b70cef1
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2023-05-27 16:53:24 +0200

    finally fixed pycache

--- src/desktop/AirFlow/init.py
+++ src/desktop/AirFlow/init.py
@@ -1,5 +1,9 @@
 import PyQt5 as ui
+import base64 as b64
+
+deConfig = input("args for starting AirFlow (separated by a comma):")
 
 def init() :
     ui.__name__("Niosem AirFlow")
     ui.__package__("niosem.airflow.linux.de.prod")
+    b64.b64encode(deConfig)
--- src/desktop/AirFlow/main.py
+++ src/desktop/AirFlow/main.py
@@ -1,3 +1,5 @@
 import init as deInit
+import os
 
-deInit.Init()
+deInit.Init(
+)
