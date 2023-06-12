0554a42f548dedc1c650e157ea25c2e226feecf1
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2023-06-12 18:01:37 +0200

    khjgjgkjk thanks fou nzeirstanding

--- src/xmdl/gethttp.py
+++ src/xmdl/gethttp.py
@@ -3,14 +3,16 @@ import os
 import time
 
 def get_package() :
-    print("niosem AIrOS package. Waiting for airPackager.niosem.sh")
-    time.sleep(4.512)
+    print("Niosem AirOS packages are not available as i do not have a domain. Help me get one for free, by giving me some addresses for getteing one. Thank you")
+    time.sleep(4.512) #Make sure manager has time to fetch response
+    #IF i get a domain, i will do `https://airpackager.mydomain.site/packages/[PACKAGENAME]
     
 
 def get_package_aur() :
     print("Aur package. Contacting aur.archlinux.org")
-    time.sleep(4.512)
+    time.sleep(4.512) #Make sure manager has time to fetch response
 
 def GetInScription() :
     print("Fetching data from InScription servers. This WILL take a few seconds")
-    time.sleep(4.512)
+    time.sleep(4.512) #Make sure manager has time to fetch response
+    #IF i get a domain, i will do `https://inscription.mydomain.site/packages/[PACKAGENAME]
